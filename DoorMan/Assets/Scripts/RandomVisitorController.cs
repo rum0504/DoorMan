412d0d66608949ae41a1d5038064bbf069280278
rum0504 <manakamm4062@gmail.com>
2024-03-09 11:50:10 +0900

    Main画面の見た目のアップデート

--- DoorMan/Assets/Scripts/RandomVisitorController.cs
+++ DoorMan/Assets/Scripts/RandomVisitorController.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class RandomVisitorController : MonoBehaviour
 {
     public GameObject[] visitorPrefabs; // 来訪者のプレハブ（複数）
-    public Vector3 spawnPosition = new Vector3(0, 0.5f, 5); // 来訪者の生成位置
-    public Vector3 doorManPosition = new Vector3(0, 0.5f, -7); // ドアマンの位置
+    public Vector3 spawnPosition = new Vector3(0, -1.5f, 5); // 来訪者の生成位置
+    public Vector3 doorManPosition = new Vector3(0, -1.5f, -7); // ドアマンの位置
     public float speed = 1.0f; // 来訪者の移動速度
 
     public GameManager gameManager; // GameManagerへの参照を保持する変数
@@ -47,8 +47,8 @@ public class RandomVisitorController : MonoBehaviour
 
         // 新しい来訪者の生成
         int randomIndex = Random.Range(0, visitorPrefabs.Length);
-        Instantiate(visitorPrefabs[randomIndex], spawnPosition, Quaternion.identity);
+        GameObject newVisitor = Instantiate(visitorPrefabs[randomIndex], spawnPosition, Quaternion.identity);
+        // 来訪者のRotationをy=180に固定する
+        newVisitor.transform.rotation = Quaternion.Euler(0, 180, 0);
     }
-
-
 }
